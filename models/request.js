0b4c78232415b1becb760c10653c9371205bc479
Mohamed Jasim <spk@mjcomputer.local>
2025-08-09 12:02:17 +0300

    Review

--- models/request.js
+++ models/request.js
@@ -1,6 +1,27 @@
 // models/request.js
 const mongoose = require('mongoose');
 
+const carDetailsSchema = new mongoose.Schema(
+  {
+    carType: {
+      type: String,
+      required: true
+    },
+    carModel: {
+      type: String,
+      required: true
+    },
+    carMade: {
+      type: String,
+      required: true
+    },
+    carYear: {
+      type: String,
+      required: true
+    },
+  }
+)
+
 const requestSchema = new mongoose.Schema(
   {
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
@@ -8,10 +29,7 @@ const requestSchema = new mongoose.Schema(
       type: Date,
       required: true,
     },
-    carDetails: {
-      type: String,
-      required: true,
-    },
+    carDetails: [carDetailsSchema],
     image: {
       type: String,
       required: true,
